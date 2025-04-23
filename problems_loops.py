f1a60f349d4381ad838d4da115c6f21e5b9c5a57
Sanjayarjun <gcsgdv10116@gmail.com>
2025-04-23 23:11:29 +0530

    Initial commit

---
+++ problems_loops.py
@@ -0,0 +1,60 @@
+# N = int(input("Enter Number:"))
+
+# for i in range(1, N+1):
+#     print(i)
+
+# n = int(input("Enter Number:"))
+
+# i = 1
+# while i <= n:
+#     print(i)
+#     i += 1
+
+
+
+# def sum_of_natural_numbers(N):
+#     total = 0
+#     for i in range(1, N + 1):
+#         total += i
+#     return total
+
+# # Test the function with N=5
+# N = 5
+# result = sum_of_natural_numbers(N)
+# print(f"Sum of first {N} natural numbers: {result}")
+
+
+# n = int(input("Give n Value:"))
+# i =1
+# while i<=n:
+#     if i%2 == 0:
+#         print(i)
+#     i += 1
+
+# n = int(input("Given number:"))
+# i = 1
+# while i <= n:
+#     if i % 2 != 0:
+#         print(i)
+#     i += 1
+
+# a = 3
+# for b in range(1,11):    
+#     print(f"{a}*{b} = {a*b}")
+# print("Multiplication table of 3:")
+
+
+# n = int(input("Enter a number:"))
+# i = 1
+# while i<= 10:
+#     print(n*i)
+#     i+=1
+
+
+# Factorial number 
+
+
+
+        
+
+
